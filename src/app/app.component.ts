3a84b789cbdec5bc97ebe531eaf1ace8e8d4ffb4
Kar <krlosbatdev@gmail.com>
2021-04-26 23:58:28 -0400

    add subscription to apsync

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { API } from 'aws-amplify'
+import { Observable } from 'rxjs';
 
+const query = `
+  query listWorkOrders {
+    listWorkOrders {
+      id name completed
+    }
+  }
+  `;
+const mutation = `
+  mutation createWorkOrder($workOrder: WorkOrderInput!) {
+    createWorkOrder(workOrder: $workOrder) {
+      id name completed
+    }
+  }
+`;
+const subscription = `
+  subscription onCreateWorkOrder {
+    onCreateWorkOrder {
+      id name completed
+    }
+  }
+`;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  count = 0;
+
+  ngOnInit(): void {
+    this.fetchWorkOrders();
+    this.subscribe();
+  }
   title = 'amplify-app';
+
+
+  async fetchWorkOrders() {
+    const data = await API.graphql({ query })
+    console.log('data from GraphQL:', data)
+  }
+
+  async createWorkOrder() {
+    this.count++;
+    await API.graphql({
+      query: mutation,
+      variables: { workOrder: { id: this.count, name: 'WorkOrder 1', completed: false } }
+    })
+    console.log('workOrder successfully created!')
+  }
+
+  subscribe() {
+    var subs = API.graphql({
+      query: subscription
+    }) as unknown as Observable<any>;
+
+    subs.subscribe({
+      next: workOrderData => {
+        console.log('Subscription workOrderData: ', workOrderData)
+      }
+    })
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  aws_appsync_region: environment.aws_appsync_region,
+  aws_appsync_graphqlEndpoint: environment.aws_appsync_graphqlEndpoint,
+  aws_appsync_authenticationType: environment.aws_appsync_authenticationType,
+  aws_appsync_apiKey: environment.aws_appsync_apiKey,
+});
 
 if (environment.production) {
   enableProdMode();
